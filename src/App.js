d9619f5833d7e7dd8fb1d68803ea4a9cac1ed749
Michal <michalrosa17@gmail.com>
2021-01-23 15:27:46 +0100

    add working country card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import List from './components/List';
+import Filter from './components/Filter';
+import Country from './components/Country';
+
+const API = 'https://restcountries.eu/rest/v2/all';
+
+class App extends Component {
+  regions = ['All', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  countriesList = [];
+
+  state = {
+    renderedList: [],
+    selectedRegion: '',
+  };
+
+  componentDidMount = () => {
+    fetch(API)
+      .then((response) => {
+        if (response.ok) return response.json();
+
+        throw Error(response.status);
+      })
+      .then((countriesList) => {
+        this.countriesList = countriesList;
+        this.setState({
+          renderedList: countriesList,
+        });
+      })
+      .catch((error) => console.log(error));
+  };
+
+  handleFormChange = (event) => {
+    const selectedRegion = event.target.value;
+    if (selectedRegion === 'All') {
+      this.setState({
+        renderedList: this.countriesList,
+        selectedRegion: 'All',
+      });
+    } else {
+      const renderedList = this.countriesList.filter(
+        (country) => country.region === selectedRegion
+      );
+
+      this.setState({
+        selectedRegion,
+        renderedList,
+      });
+    }
+  };
+  render() {
+    console.log(this.state.renderedList);
+    return (
+      <BrowserRouter>
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <Filter
+                regions={this.regions}
+                selectedRegion={this.state.selectedRegion}
+                handleFormChange={this.handleFormChange}
+              />
+              <List countries={this.state.renderedList} />
+            </>
+          )}
+        />
+        {this.state.renderedList.length ? (
+          <Route
+            path="/:id"
+            exact
+            render={({ match }) => (
+              <Country
+                id={match.params.id}
+                renderedList={this.state.renderedList}
+              />
+            )}
+          />
+        ) : null}
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
