39996cc4c91bdb8976972f83037248c74675c087
Michal <michalrosa17@gmail.com>
2021-10-08 13:38:50 +0200

    Add basename to BrowserRouter component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
-import Header from "./header/Header";
-import CountiresList from "./CountriesList/CountriesList";
-import Country from "./country/Country";
-import NotFound from "./notFound/NotFound";
+import Header from './header/Header';
+import CountiresList from './CountriesList/CountriesList';
+import Country from './country/Country';
+import NotFound from './notFound/NotFound';
 
 const Main = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/Front-End-Mentor-REST-Countries-API'>
       <Header />
       <Switch>
-        <Route path="/:id" exact render={() => <CountiresList />} />
-        <Route path="/:id/:type" exact render={() => <Country />} />
+        <Route path='/:id' exact render={() => <CountiresList />} />
+        <Route path='/:id/:type' exact render={() => <Country />} />
 
-        <Redirect from="/" exact to="/all" />
+        <Redirect from='/' exact to='/all' />
       </Switch>
     </BrowserRouter>
   );
